0f9078abede50bb7566c7fce92689a7769f9955f
sejjy <sejjymvm@gmail.com>
2025-08-16 14:48:27 +0800

    [feat] update subject page

--- resources/js/pages/subject.tsx
+++ resources/js/pages/subject.tsx
@@ -1,9 +1,7 @@
 import { Button } from '@/components/ui/button';
-import { Checkbox } from '@/components/ui/checkbox';
-import { Dialog, DialogContent, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogDescription, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import {
     DropdownMenu,
-    DropdownMenuCheckboxItem,
     DropdownMenuContent,
     DropdownMenuItem,
     DropdownMenuLabel,
@@ -11,7 +9,6 @@ import {
     DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import AppLayout from '@/layouts/app-layout';
@@ -29,115 +26,78 @@ import {
     useReactTable,
     VisibilityState,
 } from '@tanstack/react-table';
-import { ArrowUpDown, ChevronDown, MoreHorizontal } from 'lucide-react';
+import { MoreHorizontal } from 'lucide-react';
 import { useState } from 'react';
 
 const data: Payment[] = [
     {
-        id: 'm5gr84i9',
-        amount: 316,
-        status: 'success',
-        email: 'ken99@example.com',
+        code: 'ABC123',
+        name: 'Fuck',
+        unit: 69,
+        instructor: 'Papa Renzie',
     },
     {
-        id: '3u1reuv4',
-        amount: 242,
-        status: 'success',
-        email: 'Abe45@example.com',
+        code: 'DEF456',
+        name: 'Puta',
+        unit: 143,
+        instructor: 'Santo Papa Mark',
     },
     {
-        id: 'derv1ws0',
-        amount: 837,
-        status: 'processing',
-        email: 'Monserrat44@example.com',
-    },
-    {
-        id: '5kma53ae',
-        amount: 874,
-        status: 'success',
-        email: 'Silas22@example.com',
-    },
-    {
-        id: 'bhqecj4p',
-        amount: 721,
-        status: 'failed',
-        email: 'carmella@example.com',
+        code: 'GHI789',
+        name: 'Tite',
+        unit: 5,
+        instructor: 'Daddy Diddy',
     },
 ];
 export type Payment = {
-    id: string;
-    amount: number;
-    status: 'pending' | 'processing' | 'success' | 'failed';
-    email: string;
+    code: string;
+    name: string;
+    unit: number;
+    instructor: string;
 };
 
 export const columns: ColumnDef<Payment>[] = [
     {
-        id: 'select',
-        header: ({ table }) => (
-            <Checkbox
-                checked={table.getIsAllPageRowsSelected() || (table.getIsSomePageRowsSelected() && 'indeterminate')}
-                onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-                aria-label="Select all"
-            />
-        ),
-        cell: ({ row }) => (
-            <Checkbox checked={row.getIsSelected()} onCheckedChange={(value) => row.toggleSelected(!!value)} aria-label="Select row" />
-        ),
-        enableSorting: false,
-        enableHiding: false,
+        accessorKey: 'code',
+        header: 'Course Code',
+        cell: ({ row }) => <div className="capitalize">{row.getValue('code')}</div>,
     },
     {
-        accessorKey: 'status',
-        header: 'Status',
-        cell: ({ row }) => <div className="capitalize">{row.getValue('status')}</div>,
+        accessorKey: 'name',
+        header: 'Course Name',
+        cell: ({ row }) => <div className="capitalize">{row.getValue('name')}</div>,
     },
     {
-        accessorKey: 'email',
-        header: ({ column }) => {
-            return (
-                <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
-                    Email
-                    <ArrowUpDown />
-                </Button>
-            );
-        },
-        cell: ({ row }) => <div className="lowercase">{row.getValue('email')}</div>,
+        accessorKey: 'unit',
+        header: 'Units',
+        cell: ({ row }) => <div className="capitalize">{row.getValue('unit')}</div>,
     },
     {
-        accessorKey: 'amount',
-        header: () => <div className="text-right">Amount</div>,
-        cell: ({ row }) => {
-            const amount = parseFloat(row.getValue('amount'));
-            // Format the amount as a dollar amount
-            const formatted = new Intl.NumberFormat('en-US', {
-                style: 'currency',
-                currency: 'USD',
-            }).format(amount);
-            return <div className="text-right font-medium">{formatted}</div>;
-        },
+        accessorKey: 'instructor',
+        header: 'Instructors',
+        cell: ({ row }) => <div className="capitalize">{row.getValue('instructor')}</div>,
     },
     {
         id: 'actions',
         enableHiding: false,
-        cell: ({ row }) => {
-            const payment = row.original;
+        cell: ({}) => {
             return (
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" className="h-8 w-8 p-0">
-                            <span className="sr-only">Open menu</span>
-                            <MoreHorizontal />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                        <DropdownMenuItem onClick={() => navigator.clipboard.writeText(payment.id)}>Copy payment ID</DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>View customer</DropdownMenuItem>
-                        <DropdownMenuItem>View payment details</DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
+                <div className="flex justify-end">
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="ghost" className="h-8 w-8 p-0">
+                                <span className="sr-only">Open menu</span>
+                                <MoreHorizontal />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>View customer</DropdownMenuItem>
+                            <DropdownMenuItem>View payment details</DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
             );
         },
     },
@@ -185,10 +145,8 @@ export default function Subject() {
                     </DialogTrigger>
                     <DialogContent>
                         <DialogTitle>Add Course</DialogTitle>
-                        {/* <DialogDescription>Add course for the current curriculum.</DialogDescription> */}
+                        <DialogDescription>Add course for the current curriculum.</DialogDescription>
                         <form className="flex flex-col gap-3" action="">
-                            <Label>Course Details</Label>
-
                             <div className="flex gap-2">
                                 <Select>
                                     <SelectTrigger className="w-20">
@@ -202,58 +160,28 @@ export default function Subject() {
                                 </Select>
 
                                 <Input className="w-20" type="number" placeholder="Code"></Input>
-                                <Select>
-                                    <SelectTrigger className="w-20">
-                                        <SelectValue placeholder="Units" />
-                                    </SelectTrigger>
-                                    <SelectContent>
-                                        <SelectItem value="1">1</SelectItem>
-                                        <SelectItem value="3">3</SelectItem>
-                                        <SelectItem value="5">5</SelectItem>
-                                        <SelectItem value="6">6</SelectItem>
-                                    </SelectContent>
-                                </Select>
                             </div>
                             <Input type="text" placeholder="Course Name"></Input>
+                            <Select>
+                                <SelectTrigger className="w-20">
+                                    <SelectValue placeholder="Units" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="1">1</SelectItem>
+                                    <SelectItem value="3">3</SelectItem>
+                                    <SelectItem value="5">5</SelectItem>
+                                    <SelectItem value="6">6</SelectItem>
+                                </SelectContent>
+                            </Select>
 
-                            <Button className='mt-2'>Add</Button>
+                            <Input type="text" placeholder="Instructor"></Input>
+
+                            <Button className="mt-2">Add</Button>
                         </form>
                     </DialogContent>
                 </Dialog>
 
                 <div className="w-full">
-                    <div className="flex items-center py-4">
-                        <Input
-                            placeholder="Filter emails..."
-                            value={(table.getColumn('email')?.getFilterValue() as string) ?? ''}
-                            onChange={(event) => table.getColumn('email')?.setFilterValue(event.target.value)}
-                            className="max-w-sm"
-                        />
-                        <DropdownMenu>
-                            <DropdownMenuTrigger asChild>
-                                <Button variant="outline" className="ml-auto">
-                                    Columns <ChevronDown />
-                                </Button>
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent align="end">
-                                {table
-                                    .getAllColumns()
-                                    .filter((column) => column.getCanHide())
-                                    .map((column) => {
-                                        return (
-                                            <DropdownMenuCheckboxItem
-                                                key={column.id}
-                                                className="capitalize"
-                                                checked={column.getIsVisible()}
-                                                onCheckedChange={(value) => column.toggleVisibility(!!value)}
-                                            >
-                                                {column.id}
-                                            </DropdownMenuCheckboxItem>
-                                        );
-                                    })}
-                            </DropdownMenuContent>
-                        </DropdownMenu>
-                    </div>
                     <div className="overflow-hidden rounded-md border">
                         <Table>
                             <TableHeader>
@@ -289,9 +217,6 @@ export default function Subject() {
                         </Table>
                     </div>
                     <div className="flex items-center justify-end space-x-2 py-4">
-                        <div className="flex-1 text-sm text-muted-foreground">
-                            {table.getFilteredSelectedRowModel().rows.length} of {table.getFilteredRowModel().rows.length} row(s) selected.
-                        </div>
                         <div className="space-x-2">
                             <Button variant="outline" size="sm" onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
                                 Previous
